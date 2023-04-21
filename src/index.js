71c43ebaec70eee6a95d89a2d22f30fb78ff7594
MarcusKyung <Kyungmj@gmail.com>
2023-04-21 10:38:47 -0700

    add updated JS UI logic to display results

--- src/index.js
+++ src/index.js
@@ -14,17 +14,11 @@ function getCurrency(currencyFrom, currencyTo, currencyAmount) {
 
 // UI Logic
 function printElements(response) {
-  const resultsUl = document.createElement('ul');
-  const resultsDiv = document.getElementById('resultsDiv');
-  resultsUl.append.resultsDiv;
-  const exchangeRateResults = document.createElement('li');
-  const exchangeAmountResult = document.createElement('li');
-  exchangeRateResults.innerText = `The current exchange rate from ${response[1]} to ${response[0]} is ${response[0].conversion_rate}.`;
-
+  const exchangeRateResults = document.getElementById('exchangeRate');
+  const exchangeAmountResults = document.getElementById('exchangeResults');
+  exchangeRateResults.innerText = `The current exchange rate from ${response[1]} to ${response[2]} is ${response[0].conversion_rate}.`;
   exchangeAmountResults.innerText = `${response[3]} of ${response[1]} in ${response[2]} is ${response[0].conversion_result} `;
 }
-
-// response, currencyFrom, currencyTo, currencyAmount
 
 function printError(error) {
   document.querySelector('#resultsHeader').innerText = `there was an error accessing currency exchange data for ${error[2]} and ${error[3]}: ${error[0].status} ${error[1].error}`;
